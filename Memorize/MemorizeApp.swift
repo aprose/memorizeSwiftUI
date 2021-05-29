d297219bfa9a089c4e994e39a2c279aa434b8828
Adrian Rose <apsrose@hotmail.com>
2021-05-29 17:59:32 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Adrian Rose on 29.05.21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

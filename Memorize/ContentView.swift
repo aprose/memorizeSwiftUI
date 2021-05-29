d7f1891ee80c389675211e0998cb0f8f2fd287c3
Adrian Rose <apsrose@hotmail.com>
2021-05-29 23:47:38 +0200

    Lecture 1 Complete: Start Building App

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,14 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // something that behaves like a View
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        
+        ZStack {
+            
+            RoundedRectangle(cornerRadius: 20.0)
+                .stroke()
+                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+
+            Text("Hello World")
+                .foregroundColor(.orange)
+                .padding()
+        }
+        .padding(.horizontal)
+
+
+        
+        
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
+    
     static var previews: some View {
         ContentView()
     }
+    
+    
 }

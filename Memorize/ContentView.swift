ef863a9238d6205e0cf652fbb2a7131a49c352c0
Adrian Rose <apsrose@hotmail.com>
2021-06-03 22:36:47 +0200

    Lecture 2 : Started writing Code for Lecture 2 - First 20:52 Minutes - Start of CardView

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -12,30 +12,45 @@ struct ContentView: View {
     // something that behaves like a View
     var body: some View {
         
-        
-        ZStack {
-            
-            RoundedRectangle(cornerRadius: 20.0)
-                .stroke()
-                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-
-            Text("Hello World")
-                .foregroundColor(.orange)
-                .padding()
+        HStack {
+            CardView(isFaceUp: true)
+            CardView(isFaceUp: false)
+            CardView(isFaceUp: true)
+            CardView(isFaceUp: false)
         }
         .padding(.horizontal)
+        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
 
-
-        
-        
     }
     
+}
+
+
+struct CardView: View {
+    var isFaceUp: Bool
+    
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20.0)
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.stroke(lineWidth: 3)
+                Text("✈️").font(.largeTitle)
+            } else {
+                shape.fill()
+            }
+        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
     
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+        ContentView()
+            .preferredColorScheme(.light)
+
     }
     
     
